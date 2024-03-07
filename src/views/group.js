7befd7668ea2bbc70d47d83a8b7558d99c50fcb0
Justin San Pedro <jsanpedro@myseneca.ca>
2024-03-07 15:59:38 -0500

    cleanup

--- src/views/group.js
+++
@@ -1,8 +0,0 @@
-import React, { useState } from 'react';
-const mongoose = require('mongoose'); 
- 
-export default function Group({number}) {
-    return(
-        <h1>Group {number}</h1>
-    )
-}
