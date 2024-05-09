b6b70d14cd5e2840fc4b35ba3b9c2c3661a69dbc
Justin San Pedro <jsanpedro@myseneca.ca>
2024-05-09 19:05:12 -0400

    changed logo to white

--- src/components/common/ScreenHeader.jsx
+++ src/components/common/ScreenHeader.jsx
@@ -2,8 +2,7 @@
 import '../../styles/main.scss';
 import { Link } from "react-router-dom";
 
-import logo from '../../assets/icons/logo.jpg';
-import logo_black from '../../assets/images/ShareStyle_black.png'; 
+import logo from '../../assets/icons/logo_white.png';
 import menu from '../../assets/icons/menu.png';
 import profile from '../../assets/images/profile_default.jpg';
 
@@ -65,7 +64,7 @@ const ScreenHeader = ({isLogin=true, linkName="Login"}) => {
             <div className="container-header">
                 <div className="container-row">
                     <Link className="container-header-button" to="/">
-                        <CircleBtn iconUrl={logo_black} className={"button-header"} width={"40px"} height={"50px"} />
+                        <CircleBtn iconUrl={logo} className={"button-header"} width={"40px"} height={"50px"} />
                     </Link>
                     {  
                         isLogin &&
--- src/components/common/ScreenHeaderIn.jsx
+++ src/components/common/ScreenHeaderIn.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import { useContext } from 'react';
 import { GarmentContext } from "../../../context/garmentContext.jsx";
 import { UserContext } from "../../../context/userContext.jsx";
-import logo_black from '../../assets/images/ShareStyle_black.png';
+import logo from '../../assets/icons/logo_white.png';
 import menu from '../../assets/icons/menu.png';
 import profile from '../../assets/images/profile_default.jpg';
 import { useNavigate } from 'react-router-dom';
@@ -88,7 +88,7 @@ const ScreenHeader = ({isLogin=true, linkName="Login"}) => {
             <div className="container-header">
                 <div className="container-row">
                     <Link className="container-header-button" to="/dashboard">
-                        <CircleBtn iconUrl={logo_black} className={"button-header"} width={"30px"} height={"30px"} />
+                        <CircleBtn iconUrl={logo} className={"button-header"} width={"30px"} height={"30px"} />
                     </Link>
                     {  
                         isLogin &&
