af3c4fb7c2fb86bc82970d8038a3a7e49b4786db
Zain <Zainnn15.q@gmail.com>
2024-05-14 14:02:58 -0400

    Finished

--- src/pages/Help.jsx
+++ src/pages/Help.jsx
@@ -1,8 +1,8 @@
 // Help.jsx
 import ScreenHeaderIn from "../components/common/ScreenHeaderIn";
+
 const Help = () => {
     return (
-        
         <div className="help-page">
             <ScreenHeaderIn />
             <div className="container main">
@@ -12,33 +12,109 @@ const Help = () => {
                 <div className="container-border">
                     <h2>Contact Information</h2>
                     <hr/>
-                    <p>If you experience any trouble with your group members, have questions about the research, or any other inquiries, please contact:</p>
-                    <ul>
-                        <li><strong>Sabine Weber:</strong> <a href="mailto:Sabine.Weber@senecacollege.ca">Sabine.Weber@senecacollege.ca</a></li>
-                        <li><strong>Good2Talk Helpline:</strong> Call 1-866-925-5454 or text GOOD2TALKON to 686868</li>
-                        <li><strong>Arafat Khan:</strong> For garment-related issues, email <a href="mailto:md-arafat-ali.khan@senecapolytechnic.ca">md-arafat-ali.khan@senecapolytechnic.ca</a></li>
-                    </ul>
+                    <p>If you experience any trouble with your group members, have questions about the research or have any other inquiries, dont hesitate to contact Sabine Weber, the studys principal investigator. She will do her best to resolve any problems at <a href="mailto:Sabine.Weber@senecacollege.ca">Sabine.Weber@senecacollege.ca</a>.</p>
+                    <p>If you feel uncomfortable contacting a researcher, please get in touch with the general mental health support line for postsecondary students from Good2Talk Helpline: Call 1-866-925-5454 or text GOOD2TALKON to 686868, Ontarios 24/7</p>
+                    <p>If you have problems with your garment and need help with garment-related issues (loose buttons, broken zippers, stains, ribs, etc.), dont hesitate to get in touch with Arafat Khan at <a href="mailto:md-arafat-ali.khan@senecapolytechnic.ca">md-arafat-ali.khan@senecapolytechnic.ca</a>.</p>
                 </div>
                 <br/>
                 <br/>
                 <div className="container-border">
                     <h2>Common Questions & Answers</h2>
                     <hr/>
+                    <p>The following section will provide an overview of the most urgent questions. These questions are organized into the following topics:</p>
+                    <ul>
+                        <li>Purchasing Garments</li>
+                        <li>Garment Wear, Care & Feel</li>
+                        <li>For Group participants: Garment Exchange</li>
+                    </ul>
+                    <br/>
+                    <hr/>
                     <h3>Purchasing Garments</h3>
                     <hr/>
-                    <p>Garments must be new to track wear and tear accurately. Each participant will receive $70-80 in funds to purchase a garment, including taxes and potential shipping costs.</p>
+                    <p><strong>Does the garment have to be new, or could it be second-hand?</strong><br/>
+                    Since we want to track the wear and tear of a garment, the garment must be new, so purchasing a second-hand garment is not an option.</p>
+
+                    <p><strong>How much money can a participant spend on a garment?</strong><br/>
+                    Each participant will receive $70-80 in funds to purchase one garment. This amount includes the Harmonized Sales Tax (HST) and shipping costs if the garment is purchased online. For example, if a garment is purchased in Ontario, the HST is 13%. For a garment that costs $69, the HST would be $8.97, so the total cost comes up to $77.97. In other words, this amount is within the budget of $80. If the garment also requires shipment of $ 5.50, then the total costs would be $ 83.47, and the garment would be over the budget and not covered by our study.</p>
+
+                    <p><strong>Who pays for the garment?</strong><br/>
+                    The stylist will pay for the garments for all participants who shop with a stylist. Single participants who shop independently must pay for the garment but will be reimbursed based on the receipt. Also, single participants can sign up to go shopping with a stylist.</p>
+
+                    <p><strong>Can I use my own money and make an additional payment to increase the budget for a garment?</strong><br/>
+                    With this study, we want to compare the quality of typical fast fashion brands. If participants used the research money only as a "downpayment" to purchase a more expensive garment, it would mislead our study results. So, the answer is no.</p>
+
+                    <p><strong>Could I purchase two or more garments if I stay within the budget?</strong><br/>
+                    Yes, we would allow a participant to purchase two garments as long the person stays within the budget. We do this because retailers often offer multipacks. However, we would not support the purchase of three garments.</p>
+
+                    <p><strong>Can group members make purchases with one garment higher and another lower than the budget?</strong><br/>
+                    If 5 participants are in a group and each participant has a budget of $70-80, the group has a total budget of $350-400. Yes, group members can balance the individual budgets. However, please be aware that each person has the primary ownership of one garment; this is the garment the person will receive at the end of the study. So, you want to ensure each person receives a similar value.</p>
+
+                    <p><strong>Can the garment be on sale?</strong><br/>
+                    Fashion is price-sensitive, and garments are constantly on sale, so you can purchase a garment at a reduced sales price. In fact, the garment diary asks whether the garment was reduced or had the original price.</p>
+
+                    <p><strong>How will the payment be made?</strong><br/>
+                    If participants go shopping as a group with a stylist, the stylist will pay for all the garments. Individual participants can purchase the garment, show the garment with the price tag, and give the receipt to Sabine Weber for approval. Then, the person will be reimbursed for this garment. Reimbursement can only be done in exchange for the receipt of the garment and if the garment still has the price tag.</p>
+
+                    <p><strong>What garments can be purchased?</strong><br/>
+                    In general, participants can purchase what fits them best. However, this study looks into garments for everyday wear and excludes underwear or lingerie. Additionally, we ask participants not to purchase products made from "fake" or "vegan" leather simply because the environmental impact of these products is so huge that we don't want to increase their use. This product is usually labelled as Polyurethane (PU) or Polyvinyl chloride (PVC). Also, no genuine leather or real fur products should be purchased.</p>
+
+                    <p><strong>Where can the garments be purchased?</strong><br/>
+                    Garments can be purchased in brick-and-mortar stores or online at any retailer. However, we are in contact with different fast fashion retailers that support our project. These retailers will provide better deals for participants; see retailer list.</p>
+
+                    <p><strong>What to consider when purchasing a garment?</strong><br/>
+                    Before purchasing a garment, the participant should control the care label to make sure the garment is at least washable by hand. Garments that require only dry cleaning should not be purchased.</p>
+                    <p>Make sure the garment really fits and pleases you. Garments that are comfortable and fit well are more likely to be used. We want you to have fun with the garment.</p>
+
+                    <p><strong>Return Policy:</strong> Provide details on the return policy for purchased garments, including participant responsibilities, associated costs, and the return timeframe.</p>
+
+                    <p><strong>Can I return a garment to a retailer?</strong><br/>
+                    If you were shopping with a group and a stylist and havent paid for the garment, you can not return the garment to the retailer. If you have been shopping as a single person, have changed your mind, and want to return a purchased garment, you can do so until we have reimbursed you. Once we pay for the garment, you can not return it because you will no longer have the receipt. However, please note we are not reimbursing shipping & delivery costs.</p>
                     
                     <br/>
                     <hr/>
                     <h3>Garment Wear, Care & Feel</h3>
                     <hr/>
-                    <p>You are required to update your garments diary after each wear and care. Follow the instructions on the care label for washing.</p>
-                    
+                    <p><strong>How often do I have to wear the garment?</strong><br/>
+                    There is no rule on how often and long you should wear the garment. Treat this garment as any other garment in your closet. We want to find out how long the average garment is worn over a period of three months.</p>
+
+                    <p><strong>Do I have to update the garment diary (wear duration, care & feel) after every use?</strong><br/>
+                    For us, it is essential that you keep track of your garments diary. With the web application, we try to make this as easy and comfortable as possible. Please try to fill it out after every wear and care. We are sure it is also fun for you to see the results at the end of the study.</p>
+
+                    <p><strong>When do I have to wash the garment?</strong><br/>
+                    The answer is it depends. If you are an individual participant, you can wash the garment based on your usual washing habits. If you are an individual participant, we ask you to wash the garment before you pass it on to a group member. Different understandings of cleanness can be a major source of stress among group members; please help to mitigate this by washing your garment before passing it on.</p>
+
+                    <p><strong>How do I have to wash the garment?</strong><br/>
+                    Every garment has a wash and care label, usually in the left side seam or a pocket. Please follow these instructions. However, you could also use an option for a more delicate garment. For example, instead of washing your garment with a machine at a medium temperature, you could also wash it at a low temperature by hand.</p>
+
+                    <p><strong>Can I use a clothing dryer?</strong><br/>
+                    Yes, you can if you follow the care instructions and do not put a garment into a dryer that is not suitable or expose it to a higher temperature than indicated on the care label.</p>
+
+                    <p><strong>What laundry detergent do I have to use?</strong><br/>
+                    You can use your regular laundry detergent if you are an individual participant. For group participants, this is different. Many people have allergies, but at the same time, companies must only provide ingredients of a product with more than 3 percent. This means fragrances are usually not reported, so we can not determine what critical ingredients may be used in a laundry detergent. To protect all participants, we ask all group members to only use Fragrance-Free Detergent, for example, from Tru Earth. Hypoallergenic laundry detergents are better for sensitive skin and reduce the risk of allergies. We are currently investigating if we can provide participants with laundry detergent.</p>
+
+                    <p><strong>What should I do if I spoil a garment with stains or if something breaks on the garment?</strong><br/>
+                    Please email Arafat, who is part of the research team, to help you with all garment-related issues (loose buttons, broken zippers, stains, ribs etc.). He will do whatever is possible to help solve the problem at <a href="mailto:md-arafat-ali.khan@senecapolytechnic.ca">md-arafat-ali.khan@senecapolytechnic.ca</a>.</p>
+                    <p>You will also find his email under the helpline on the web application.</p>
+
                     <br/>
                     <hr/>
                     <h3>For Group Participants: Garment Exchange</h3>
                     <hr/>
-                    <p>Garment exchange schedules are flexible; communicate with your group to manage exchanges effectively.</p>
+                    <p><strong>When do we need to exchange garments?</strong><br/>
+                    There is no rule that says you must exchange garments every week or every other. Instead, we leave it up to the group members to experience their rhymes. However, we want to encourage you to exchange the garments as you wish to find out if clothing sharing is a viable option for you in the future. The web application has a function that shows when your group members are at Seneca. This feature should help you make the exchange easy.</p>
+
+                    <p><strong>Do I have to exchange my garment when a group member wants to wear it?</strong><br/>
+                    The web application has a function where group members can reserve a garment, for example, for a special event. This feature should help you ensure two participants are not planning to wear the garment on the same day – this is a first come, first serve solution, and it relies on all participants to work together. The research team does not monitor any exchange requests.</p>
+
+                    <p><strong>How can group members communicate with each other?</strong><br/>
+                    We leave it up to the participants to communicate with each other. We will connect you via email and use email to reach out to you to organize a shopping date and time, but we will not ask you, for example, for your phone number. If a team wants to use WhatsApp, please go ahead, but the research team will not be involved.</p>
+
+                    <p><strong>Is there a possibility to share more than one garment with my group members?</strong><br/>
+                    The answer is yes; if your group enjoys sharing garments and the system functions well, each member can add an additional four garments to the web application. This function means the group could share up to 25 garments and explore clothing sharing on a master level. However, please note that the research team is not paying for the additional clothes and is not responsible for repairing and maintaining these garments. Each participant will contribute their garments for exchange at their own risk.</p>
+
+                    <p><strong>What should I do if I do not want to participate anymore in the study?</strong><br/>
+                    Please note participation in this study is entirely voluntary, and if you choose not to participate or to withdraw, it will not impact your relationship with Seneca Polytech.</p>
+                    <p>If you are no longer interested in participating after purchasing the garment, you can not withdraw consent without having had a conversation with the researcher, Sabine Weber, at <a href="mailto:Sabine.weber@senecapolytechnic.ca">Sabine.weber@senecapolytechnic.ca</a>. She will also inform potential group members and ensure they can continue the study.</p>
                 </div>
             </div>
         </div>
