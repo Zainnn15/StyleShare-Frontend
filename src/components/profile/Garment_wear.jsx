ac46ea972b6b9206d1152f4f9316760c0dcbd0c1
Phan Trung Kien <kevinkien2706@gmail.com>
2024-07-03 22:39:40 -0400

    Update Delete & Edit button

--- src/components/profile/Garment_wear.jsx
+++ src/components/profile/Garment_wear.jsx
@@ -1,51 +1,63 @@
 /* eslint-disable react/prop-types */
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { formatDate, getImageFromURL } from '../../constants/functions/valueHandlers.jsx';
 import '../../styles/main.scss';
 
-const Garment_wear = ({garment}) => {
-    return(
+const Garment_wear = ({ garment }) => {
+    const [wearInfo, setWearInfo] = useState(garment ? garment.wearInfo : []);
+    const navigate = useNavigate();
+
+    const handleEdit = () => {
+        navigate(`/garment-wear/`);
+    };
+
+    const handleDelete = (id) => {
+        const updatedWearInfo = wearInfo.filter(wear => wear._id !== id);
+        setWearInfo(updatedWearInfo);
+    };
+
+    return (
         <div className="m1">
             {
-                garment && garment.wearInfo.map((wear)=>{
-                return (
+                wearInfo.map((wear) => (
                     <div key={wear._id}>
                         <label className="container-subtitle-2">{formatDate(wear.wearDate)}</label>
                         <div className="container-grid-3-md gap container-border clear-box">
                             <div>
                                 <p>
-                                <label className="text-b">Wear Time (hours):<label className="tab"></label></label>
-                                {wear.wearTime}
+                                    <label className="text-b">Wear Time (hours):<label className="tab"></label></label>
+                                    {wear.wearTime}
                                 </p>
                             </div>
                             <div>
                                 <p>
-                                <label className="text-b">Front Photo:<label className="tab"></label></label>
+                                    <label className="text-b">Front Photo:<label className="tab"></label></label>
                                 </p>
                                 <div className='container-input-img img-size-sm'>
-                                    <img src={getImageFromURL(wear.wearFront)} alt="front"/>
+                                    <img src={getImageFromURL(wear.wearFront)} alt="front" />
                                 </div>
                             </div>
                             <div>
                                 <p>
-                                <label className="text-b">Back Photo:<label className="tab"></label></label>
+                                    <label className="text-b">Back Photo:<label className="tab"></label></label>
                                 </p>
                                 <div className='container-input-img img-size-sm'>
-                                    <img src={getImageFromURL(wear.wearBack)} alt="back"/>
+                                    <img src={getImageFromURL(wear.wearBack)} alt="back" />
                                 </div>
                             </div>
                         </div>
                         <div>
-                            <button>Edit</button>
-                            <button>Delete</button>
+                            <button className="button-regular" style={{ margin: '5px' }} onClick={() => handleEdit()}>Edit</button>
+                            <button className="button-regular" onClick={() => handleDelete(wear._id)}>Delete</button>
                         </div>
-                        <br/>
+                        <br />
                     </div>
-                )})
+                ))
             }
-            
-            <br/>
+            <br />
         </div>
-    )
+    );
 }
 
 export default Garment_wear;
